37ae2a5c54f3f7c135edbc99443e4dfaaf4be813
Raul Rzayev <raulrza01@gmail.com>
2022-10-18 23:09:25 +0200

    Update Solution.java

--- Algorithm I/RotateArray/src/Solution.java
+++ Algorithm I/RotateArray/src/Solution.java
@@ -1,10 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int numsSize = nums.length
-        for (int i = 0; i < numsSize; i++) {
-            int tmp = nums[(i+k)%numsSize];
-            nums[(i+k)%numsSize] = nums[i];
-            
+        int numSize = nums.length;
+        k %= numSize;
+        reverse(nums, 0, numSize - 1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, numSize - 1);
+    }
+
+    public void reverse(int[] nums, int left, int right){
+        while(left <= right){
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
         }
     }
 }
